cbcb6069d098d739514cf11b544026e18e15d267
Marlon Pons Blanco <mapachestropicales@gmail.com>
2025-05-01 12:01:24 +0000

    proyecto finalizado

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-/*Logica para cambiar el color en los selectores de cambio de vista*/
 
 const buttonsView = document.querySelectorAll('.viewSingleTable')
+const btnList = document.getElementById("view-list");
+const btnGrid = document.getElementById("view-grid");
+const postContainer =document.getElementById("post-container");
+const posts = document.querySelectorAll('.post');
+/*variables para quitar el texto de los post usando display none*/
+const postTexts = document.querySelectorAll('.card-text');
+const postHeader= document.querySelectorAll('.card-header');
+/*variable para cambiar el estilo de las fotos*/
+const postImages = document.querySelectorAll(".post-img");
 
+/*para que cuando se cargue la pagina por primera vez se muestre en view-list simulando un click en el boton list*/
+window.addEventListener('DOMContentLoaded', () => {
+    btnList.click();
+});
+
+/*Logica para cambiar el color en los selectores de cambio de vista*/
 buttonsView.forEach(button=>{
     //evento de click a cualquiera de los dos botones para cambiar la vista
     button.addEventListener('click',()=>{
@@ -12,36 +26,34 @@ buttonsView.forEach(button=>{
     });
 });
 
+
 /*logica para cambiar la vista de forma de lista a forma de cuadricula*/
-
-const btnList = document.getElementById("view-list");
-const btnGrid = document.getElementById("view-grid");
-const posts = document.querySelectorAll('.post');
-const postTexts = document.querySelectorAll('.card-text');
-const postHeader= document.querySelectorAll('.card-header');
-
 btnList.addEventListener('click', ()=>{
     posts.forEach(e => {
         e.classList.remove("col-md-4");
         e.classList.add("col-12")
     });
-    postTexts.forEach(text => {
-        text.classList.remove("d-none");
+
+    postContainer.classList.add("d-flex","flex-column","align-items-center");
+    postTexts.forEach(text => text.classList.remove("d-none"));
+    postHeader.forEach(header => header.classList.remove("d-none"));
+    postImages.forEach(img => {
+        img.classList.remove("ratio", "ratio-1x1");
     });
-    postHeader.forEach(header=>{
-        header.classList.remove("d-none");
-    });
-})
+});
+
 
 btnGrid.addEventListener('click',()=>{
     posts.forEach(e=>{
         e.classList.remove("col-12");
         e.classList.add("col-md-4");
     });
-    postTexts.forEach(text => {
-        text.classList.add("d-none");
-    });
-    postHeader.forEach(header=>{
-        header.classList.add("d-none");
+
+    postContainer.classList.remove("d-flex","flex-column","align-items-center");
+    postTexts.forEach(text => text.classList.add("d-none"));
+    postHeader.forEach(header => header.classList.add("d-none"));
+    postImages.forEach(img => {
+        img.classList.add("ratio","ratio-1x1"); //Hace las imagenes cuadradas usando ratio y ratio-1x1
     });
 })
+
